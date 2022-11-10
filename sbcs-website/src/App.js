0b27c64d88954f993de10d43e2eb57f6330a8d3a
Angel Xie <jxie21@stuy.edu>
2022-11-09 21:21:15 -0500

    installed tailwind

--- sbcs-website/src/App.js
+++ sbcs-website/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Hello :)</p>
+      <p className="bg-blue-700">Hello :)</p>
       <p>hello this is part 2</p>
     </div>
   );
