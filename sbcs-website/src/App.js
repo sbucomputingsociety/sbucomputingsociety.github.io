7011353a94209d85171d872617612c934e9f0d03
unknown <kyoshisew@gmail.com>
2022-11-09 21:25:25 -0500

    added button

--- sbcs-website/src/App.js
+++ sbcs-website/src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <p className="bg-blue-700">Hello :)</p>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Button
+      </button>
       <p>hello this is part 2</p>
     </div>
   );
