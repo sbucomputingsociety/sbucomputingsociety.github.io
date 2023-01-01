03c1f6cc69d8cbb74127aaaef15e78a96f39133b
Angel Xie <jxie21@stuy.edu>
2023-01-01 13:04:44 -0500

    added css to navbar

--- sbcs-website/src/components/NavBar.js
+++ sbcs-website/src/components/NavBar.js
@@ -1,11 +1,19 @@
+import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import './NavBar.css';
 
 function NavBar() {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        // each time this function is called, we add or remove this className from the nav tag
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
     return (
         <header>
-            <img src='logos/SBCSlogo-removebg-preview.png' alt='logo'></img>
-            <nav>
+            <img className="logo scale-50" src='logos/SBCSlogo-removebg-preview.png' alt='logo'></img>
+            <nav ref={navRef}>
                 <a href="/#">Home</a>
                 <a href="/#">Team</a>
                 <a href="/#">Events</a>
@@ -13,12 +21,12 @@ function NavBar() {
                 <a href="/#">Sponsors</a>
                 <a href="/#">Contact</a>
 
-                <button>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
                     <FaTimes/>
                 </button>
             </nav>
 
-            <button>
+            <button className="nav-btn" onClick={showNavbar}>
                 <FaBars />
             </button>
 
