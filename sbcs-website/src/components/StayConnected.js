ca5575adcbf601bcce2f7cfe0c37284a31a14983
Angel Xie <jxie21@stuy.edu>
2023-01-10 11:01:56 -0500

    added all past event posters + created stay connected section

---
+++ sbcs-website/src/components/StayConnected.js
@@ -0,0 +1,33 @@
+import './StayConnected.css';
+
+const posts = [
+    {image:'posters/gitworkshop.png',link:'https://www.instagram.com/p/ClhadQ9uDbI/?utm_source=ig_web_copy_link'},
+    {image:'posters/careers_in_Tech_11_9_2022.png',link:'https://www.instagram.com/p/Ckgm158O7Db/?utm_source=ig_web_copy_link'},
+    {image:'posters/Technical_Interview_Prep_11_2_2022.png',link:'https://www.instagram.com/p/CkTNxqdOwaO/?utm_source=ig_web_copy_link'},
+    {image:'posters/Intro_to_web_dev_9_28_2022.png',link:'https://www.instagram.com/p/Ci77nVfu7am/?utm_source=ig_web_copy_link'},
+    {image:'posters/succulent.png',link:'https://www.instagram.com/p/CdzAqdQF6fM/?utm_source=ig_web_copy_link'},
+    {image:'posters/SBCS_Mentorship_Program_Flyer.png',link:'https://www.instagram.com/p/CisE83Su_Xw/?utm_source=ig_web_copy_link'},
+
+]
+
+function StayConnected(){
+    return(
+
+        <div>
+            <h1 className="sec-title">Stay Connected</h1>
+            <h4 className ="sec-text">Follow us on Instagram and join our discord server to get updated on our events, programs, and resources</h4>
+            <div className="post-box">
+            {posts.map(i => (
+                <div className="each-post">
+                    <a href={i.link}>
+                        <img src={i.image} alt='posts'/>
+                    </a>
+                </div>
+            ))} 
+          </div>
+        </div>
+
+    );
+}
+
+export default StayConnected;
