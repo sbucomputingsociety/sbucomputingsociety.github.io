97f7fc73667e6dfc08c204f1e15bdb09a788b73b
Angel Xie <jxie21@stuy.edu>
2023-02-11 16:59:44 -0500

    fixed button css

--- sbcs-website/src/components/Intro.js
+++ sbcs-website/src/components/Intro.js
@@ -6,7 +6,8 @@ function Intro() {
             <div className="intro-text">
                 <h1 className="text-left pb-5 text-7xl font-normal text-lighter-gray">We are the <span className="font-bold">Stony Brook Computing Society</span> _</h1>
                 <h4 className="text-left pb-5 pr-20 text-2xl text-base font-normal text-lighter-gray">The Stony Brook Computing Society is a student organization at Stony Brook University created for all students who are interested in programming, computers, and related technology.</h4>
-                <a className="py-3 px-7 mr-auto text-2xl font-bold border-5 bg-main-gray border-black inline-block leading-tight shadow-md hover:bg-hover-gray hover:shadow-lg focus:bg-hover-gray focus:shadow-lg focus:outline-none focus:ring-0 active:bg-hover-gray active:shadow-lg transition duration-150 ease-in-out" href="/#">Get Involved</a>
+                {/* <a className="py-3 px-7 mr-auto text-2xl font-bold border-5 bg-main-gray border-black inline-block leading-tight shadow-md hover:bg-hover-gray hover:shadow-lg focus:bg-hover-gray focus:shadow-lg focus:outline-none focus:ring-0 active:bg-hover-gray active:shadow-lg transition duration-150 ease-in-out" href="/#">Get Involved</a> */}
+                <button className="get-involved-btn">Get Involved</button>
             </div>
             <img className="computer-logo" src="logos/web-logo-b-cropped.png" alt="logo"></img>
         </div>
