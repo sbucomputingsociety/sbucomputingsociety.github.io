4612bb6deec3f7e852765cd0230b227fac2fd69d
Angel Xie <jxie21@stuy.edu>
2023-02-11 17:19:17 -0500

    added html for event page

---
+++ sbcs-website/src/components/EventPage.js
@@ -0,0 +1,76 @@
+import './EventPage.css';
+import NavBar from './NavBar';
+import Footer from './Footer';
+import Events from './Events';
+
+const pastEvents = [
+
+    {
+        title: 'Stony Brook Involvement Fair',
+        image: 'posters/involvement1.png',
+        date: 'Wed, Sept 1, 2021 12:30PM - 2:30PM',
+        location: 'SAC Plaza',
+        description: 'Are you looking to join a CS community? The SBCS will be participating in the CEAS Involvement Fair this coming week. We will be sharing club initiatives and giving out snakcs! Come to meet our board members and learn about ways in which you can participate in our club.',
+      },
+      {
+        title: 'CEAS Involvement Fair',
+        image: 'posters/involvement2.png',
+        date: 'Wed, Sept 8 2021 12:30PM - 2:30PM',
+        location: 'Engineering Quad',
+        description: 'Are you looking to join a CS community? The SBCS will be participating in the CEAS Involvement Fair this coming week. We will be sharing club initiatives and giving out snakcs! Come to meet our board members and learn about ways in which you can participate in our club.',
+      },
+      {
+        title: 'CEAS Involvement Fair',
+        image: 'posters/involvement2.png',
+        date: 'Wed, Sept 8 2021 12:30PM - 2:30PM',
+        location: 'Engineering Quad',
+        description: 'Are you looking to join a CS community? The SBCS will be participating in the CEAS Involvement Fair this coming week. We will be sharing club initiatives and giving out snakcs! Come to meet our board members and learn about ways in which you can participate in our club.',
+      },
+      {
+        title: 'CEAS Involvement Fair',
+        image: 'posters/involvement2.png',
+        date: 'Wed, Sept 8 2021 12:30PM - 2:30PM',
+        location: 'Engineering Quad',
+        description: 'Are you looking to join a CS community? The SBCS will be participating in the CEAS Involvement Fair this coming week. We will be sharing club initiatives and giving out snakcs! Come to meet our board members and learn about ways in which you can participate in our club.',
+      },
+      {
+        title: 'CEAS Involvement Fair',
+        image: 'posters/involvement2.png',
+        date: 'Wed, Sept 8 2021 12:30PM - 2:30PM',
+        location: 'Engineering Quad',
+        description: 'Are you looking to join a CS community? The SBCS will be participating in the CEAS Involvement Fair this coming week. We will be sharing club initiatives and giving out snakcs! Come to meet our board members and learn about ways in which you can participate in our club.',
+      },
+      {
+        title: 'CEAS Involvement Fair',
+        image: 'posters/involvement2.png',
+        date: 'Wed, Sept 8 2021 12:30PM - 2:30PM',
+        location: 'Engineering Quad',
+        description: 'Are you looking to join a CS community? The SBCS will be participating in the CEAS Involvement Fair this coming week. We will be sharing club initiatives and giving out snakcs! Come to meet our board members and learn about ways in which you can participate in our club.',
+      },
+]
+
+function EventPage() {
+    return (
+        <div>
+            <NavBar></NavBar>
+
+            {/* upcoming events */}
+            <Events></Events>
+
+            {/* past events */}
+            <div className="past-events">
+                <PastEventCard
+                    image={i.image}
+                    title={i.title}
+                    date={i.date}
+                    location={i.location}
+                    description={i.description}
+                ></PastEventCard>
+            </div>
+
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default EventPage;
