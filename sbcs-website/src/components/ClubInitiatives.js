28975512a027c2c51101858405e5bcdc59eccedb
Angel Xie <jxie21@stuy.edu>
2023-02-11 17:10:17 -0500

    allow initiative cards to take in links

--- sbcs-website/src/components/ClubInitiatives.js
+++ sbcs-website/src/components/ClubInitiatives.js
@@ -3,16 +3,19 @@ import InitCards from './InitCards';
 
 const inits = [
     {
+        link: '#',
         image: 'posters/GBM_1_Intro_9_14_2022.jpg',
         title: 'Technical Workshops',
         description: 'Led by eboard memebrs, these meetings introduce cs concepts and skillsets relavent to career success.'
     },
     {
+        link: '#',
         image: 'posters/SBCS_Mentorship_Program_Flyer.png',
         title: 'Special Programs',
         description: 'Striving to share resources, we offer mentorships that connect members with experienced alumini.'
     },
     {
+        link: '#',
         image: 'pictures/gallary/Roth3.jpg',
         title: 'Club Events',
         description: 'SBCS work with clubs and faculty members to host guest speakers, competitions, socials, and more!'
--- sbcs-website/src/components/InitCards.js
+++ sbcs-website/src/components/InitCards.js
@@ -3,13 +3,15 @@ import './InitCards.css';
 function InitCards(props){
     return(
         <div className="init-card-item">
-            <div className="init-card">
-                <img className="init-img" src={props.image} alt="club initiative poster"></img>
-                <div className="init-info">
-                    <h4 className="init-title">{props.title}</h4>
-                    <h4 className="init-description">{props.description}</h4>
+            <a href={props.link}>
+                <div className="init-card">
+                    <img className="init-img" src={props.image} alt="club initiative poster"></img>
+                    <div className="init-info">
+                        <h4 className="init-title">{props.title}</h4>
+                        <h4 className="init-description">{props.description}</h4>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     )
 }
