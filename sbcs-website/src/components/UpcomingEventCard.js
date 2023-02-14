39d091a272a2c1f3bf827af1aa3c4f8470906738
Angel Xie <jxie21@stuy.edu>
2023-02-13 23:36:12 -0500

    fixed upcoming event card alignment in event page

--- sbcs-website/src/components/UpcomingEventCard.js
+++ sbcs-website/src/components/UpcomingEventCard.js
@@ -3,11 +3,13 @@ import './UpcomingEventCard.css';
 function UpcomingEventCard(props) {
     return(
         <div className="init-card-item">
-            <img className="init-img" src={props.image} alt="poster"></img>
-            <div className="upcoming-event-box-p">
-                <h1 className="upcoming-event-title-p">{props.title}</h1>
-                <h2 className="upcoming-event-log-p-blue">{props.date}</h2>
-                <h2 className="upcoming-event-log-p-black">{props.location}</h2>
+            <div className="upcoming-event-card-container">
+                <img className="init-img" src={props.image} alt="poster"></img>
+                <div className="upcoming-event-box-p">
+                    <h1 className="upcoming-event-title-p">{props.title}</h1>
+                    <h2 className="upcoming-event-log-p-blue">{props.date}</h2>
+                    <h2 className="upcoming-event-log-p-black">{props.location}</h2>
+                </div>
             </div>
         </div>
     )
