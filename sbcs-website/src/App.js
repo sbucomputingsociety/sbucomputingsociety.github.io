5e0c41063563e9cc84d3a702b57285f1fefb6cc3
Angel Xie <jxie21@stuy.edu>
2023-02-17 15:26:11 -0500

    added animation to intro

--- sbcs-website/src/App.js
+++ sbcs-website/src/App.js
@@ -15,16 +15,12 @@ import Team from './components/Team';
 import Sponsors from './components/Sponsors'
 import EventPage from './components/EventPage';
 import Contact from './components/Contact';
-import Aos from "aos";
-import "aos/dist/aos.css";
-import {useEffect} from "react";
+
 
 
 function App() {
 
-  useEffect(() => {
-    Aos.init({duration: 2000});
-  }, []);
+  
 
   // Upcoming events
   const events = [
@@ -58,9 +54,7 @@ function App() {
       <div className="divider-brn"></div>
 
       {/* Introduction Section */}
-      <div data-aos="fade-up">
       <Intro></Intro>
-      </div>
       <div className="divider-wht"></div>
       <YellowStripe></YellowStripe>
       <div className="divider-wht"></div>
