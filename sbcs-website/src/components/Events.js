0fb349dbaeb50389533668550a21f814e73a45e9
Angel Xie <jxie21@stuy.edu>
2023-02-18 00:22:26 -0500

    working on animation...

--- sbcs-website/src/components/Events.js
+++ sbcs-website/src/components/Events.js
@@ -1,24 +1,32 @@
 import './Events.css';
 import EventCard from './EventCard';
 
+import Aos from "aos";
+import "aos/dist/aos.css";
+import {useEffect} from "react";
 
 function Events(props) {
-    return (
-        <div className="upcoming-box">
-          <h1 className="home-event-title">Upcoming</h1>
-          <div className="home-event-card divide-y-2 divide-black">
-            {props.item.map(i => (
-              <EventCard
-                image={i.image}
-                title={i.title}
-                date={i.date}
-                location={i.location}
-                description={i.description}
-              />
-            ))} 
-          </div>       
-        </div>
-    )
+
+  useEffect(() => {
+    Aos.init({duration: 2000});
+  }, []);
+
+  return (
+      <div className="upcoming-box">
+        <h1 className="home-event-title">Upcoming</h1>
+        <div className="home-event-card divide-y-2 divide-black" data-aos="fade-left">
+          {props.item.map(i => (
+            <EventCard
+              image={i.image}
+              title={i.title}
+              date={i.date}
+              location={i.location}
+              description={i.description}
+            />
+          ))} 
+        </div>       
+      </div>
+  )
 }
 
 export default Events;
